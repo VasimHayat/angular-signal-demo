f3f9ea50b38a1ef47e62c110f47bdff5c92b76a1
mvasim <mvasim>
2023-04-04 04:53:42 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
-
-@Component({
+ 
+import { Component, signal, computed, effect } from '@angular/core';
+@Component({ 
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'signal-demo';
+  counter = signal(0);
+
+  constructor() {
+    effect(() => {
+      console.log('called', this.counter());
+    });
+
+    // sync updates send push notifications
+    // to the consumer effect() because it references counter()
+    this.counter.set(1); 
+    this.counter.update((current) => current + 1);
+    this.counter.update((current) => current + 1);
+    // the effect starts as dirty
+    // and runs during change detection (refresh view)
+    // and will therefore log only once with value of 4
+  }
+
+  update() {
+    // repeated calls to the set will send push notification
+    // about counter signal producer might have changed
+    // effect then runs as click effect also triggers CD
+    // effect polls producer if value has changed
+    this.counter.update((current) => current + 1);
+    this.counter.update((current) => current + 1);
+    this.counter.update((current) => current + 1);
+    // the value has changed so effect will run on every click
+    // effect will print only single log statement per click
+    // with the prev value + 3 because it only pulls the
+    // current value of the counter at the time it runs (during CD)
+  }
+
+  updateToSameValue() {
+    // repeated calls to the set  (and update) will NOT send
+    // push notificatin about counter signal producer might have changed
+    // effect then runs as click effect also triggers CD
+    // effect polls producer if value has changed
+    this.counter.set(100);
+    setTimeout(() => {
+      this.counter.set(200);
+    }, 2000);
+
+    this.counter.set(300);
+    // this.counter.update((current) => 100);
+    // only first call will log because after that
+    // running effect will determine that producer value
+    // has not changed even though it was notified
+    // and there is a CD because of subsequent events
+  }
 }
